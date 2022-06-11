572b76ed107cb9daafffd89ad172fe9b3f096d66
Alejandro <alejandroalicontrerasdiaz@gmail.com>
2022-06-11 03:06:16 -0300

    Replacing localhost links after deploy

--- src/components/Redirection/Redirection.js
+++ src/components/Redirection/Redirection.js
@@ -6,17 +6,20 @@ const Redirection = () => {
 
     useEffect(() => {
 
-        // después hacer que esto sea guarde en localStorage y redireccione a Dashboard :) 
+        // después hacer que esto sea guarde en localStorage y redireccione a Dashboard 
         const code = window.location.hash.split("#")[1].split('=')[1]
         localStorage.setItem('token', code)
 
         setTimeout(() => {
-            window.location = '/dashboard'
+            window.location = 'https://sponify-app.netlify.app/dashboard'
         }, 1500)
     }, [])
 
     return (
         <>
+
+
+
       
       <div className='loading'>
         <div class="load-3">
